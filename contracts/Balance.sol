794b3a350d471674682853481b68209aef6d1403
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2024-02-11 12:47:39 +0100

    Added access control

--- contracts/Balance.sol
+++ contracts/Balance.sol
@@ -3,13 +3,15 @@ pragma solidity ^0.8.9;
 // import "hardhat/console.sol";
 
 contract Balance {
-    uint public balance;
-    event Withdrawal(uint amount, uint when);
-    constructor() {
-        balance = 0;
+    address public owner;
+    uint public balance; 
+    constructor(){
+        owner = msg.sender;
     }
+    event Withdrawal(uint amount, uint when);
 
     function withdraw(uint _amount) public payable {
+        require(msg.sender == owner, "Only owner can withdraw");
         require(msg.value >= _amount, "Insufficient funds");
         balance += _amount;
         emit Withdrawal(_amount, block.timestamp);
