3924bb39cc813afe471d76107e38058330cf2e28
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2024-02-14 12:12:06 +0100

    Deployement scripts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,22 +7,16 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = hre.ethers.parseEther("0.001");
-
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+  const BalanceFactory = await hre.ethers.getContractFactory("Balance");
+  const ExternalCallerRecorderFactory = await hre.ethers.getContractFactory(
+    "ExternalCallerRecorder"
   );
+  const externalCaller = await ExternalCallerRecorderFactory.deploy();
+  await externalCaller.deployed();
+  console.log("ExternalCallerRecorder deployed to:", externalCaller.address);
+  const balance = await BalanceFactory.deploy();
+  await balance.deployed();
+  console.log("Balance deployed to:", balance.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
